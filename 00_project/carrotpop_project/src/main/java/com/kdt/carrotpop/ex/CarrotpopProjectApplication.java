5af420b5c680464ef8259c9f981860d83c803469
KeyvanOh <156261972+KeyvanOh@users.noreply.github.com>
2024-05-01 01:01:35 -0700

    Add files via upload

---
+++ 00_project/carrotpop_project/src/main/java/com/kdt/carrotpop/ex/CarrotpopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.carrotpop.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotpopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotpopProjectApplication.class, args);
+	}
+
+}
---
+++ 00_project/carrotpop_project/src/test/java/com/kdt/carrotpop/ex/CarrotpopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.carrotpop.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotpopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
